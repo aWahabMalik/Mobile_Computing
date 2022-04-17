a1825cdd1e8f2bf0921409b3c49750cb9c34e112
unknown <abdulwahabmalik999@gmail.com>
2022-04-17 23:28:04 +0500

    Added functionality to button

--- Studio_Work/MakingButton/app/src/main/java/com/example/addbutton/MainActivity.java
+++ Studio_Work/MakingButton/app/src/main/java/com/example/addbutton/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.addbutton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onclickButton(View view){
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Hello Wahab");
     }
 }
