618d5d4605ac67f478b16ee0d5d4b02f3879b2d5
unknown <abdulwahabmalik999@gmail.com>
2022-05-23 10:28:28 +0500

    Assignment

--- Studio_Work/7_Assignment/app/src/main/java/com/example/assignment/MainActivity.java
+++ Studio_Work/7_Assignment/app/src/main/java/com/example/assignment/MainActivity.java
@@ -24,13 +24,54 @@ public class MainActivity extends AppCompatActivity {
 
     public void digitClick(View view)
     {
+        switch (view.getId())
+        {
+            case R.id.b0:
+                openActivity("zero");
+                break;
+            case R.id.b1:
+                openActivity("one");
+                break;
+            case R.id.b2:
+                openActivity("two");
+                break;
+            case R.id.b3:
+                openActivity("three");
+                break;
+            case R.id.b4:
+                openActivity("four");
+                break;
+            case R.id.b5:
+                openActivity("five");
+                break;
+            case R.id.b6:
+                openActivity("six");
+                break;
+            case R.id.b7:
+                openActivity("seven");
+                break;
+            case R.id.b8:
+                openActivity("eight");
+                break;
+            case R.id.b9:
+                openActivity("nine");
+                break;
 
+        }
     }
 
     public void openWebPage(String url)
     {
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void openActivity(String imgNo)
+    {
+        Intent intent = new Intent(getApplicationContext(), ImgActivity.class);
+        intent.putExtra("message_key", imgNo);
+        // start the Intent
         startActivity(intent);
     }
 }
